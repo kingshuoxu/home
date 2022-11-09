ee83afad23bd917d1ba544ec419b70e8e4b05538
Jingtianer <1043672911@qq.com>
2022-11-09 14:08:51 +0800

    Site updated: 2022-11-09 14:08:51

--- js/clickClipBoard.js
+++ js/clickClipBoard.js
@@ -1,11 +1,10 @@
-var buttons = document.getElementsByClassName("button");
 
-function copytoclipboard(text) {
-    console.log(text)
+
+function copyToClipboard(text, callBack) {
     navigator.clipboard.writeText(text).then(
         value => {
             // fulfillment
-            alert("copied to clipboard")
+            callBack()
         }, reason => {
             // rejection
             alert("fail: " + reason)
@@ -13,12 +12,63 @@ function copytoclipboard(text) {
     )
 }
 
-for (let i = 0; i < buttons.length; i++) {
-    if (buttons[i].getAttribute("title") == "RSS") {
-        var url = document.baseURI + "atom.xml"
-        buttons[i].href = "javascript:;"
-        buttons[i].target = "_self"
-        buttons[i].setAttribute('onclick', "copytoclipboard(\"" + url + "\");")
+
+$(document).ready(function() {
+    $(".copy").click(function() {
+        copyCode(this)
+    })
+    var buttons = document.getElementsByClassName("button")
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].getAttribute("title") == "RSS") {
+            var url = document.baseURI + "atom.xml"
+            buttons[i].href = "javascript:;"
+            buttons[i].target = "_self"
+            buttons[i].setAttribute('onclick', "copyToClipboard(\"" + url + "\, function() { alert(\"copied to clipboard\")}\");")
+            break
+        }
     }
+})
+
+
+
+function copyCode(element) {
+    if (element instanceof Element) {
+        var elemt = element
+        while(elemt.tagName != "FIGURE") {
+            elemt = elemt.parentElement
+        }
+        elemt = elemt.getElementsByClassName("code")[0]
+        
+        var text =elemt.innerText
+        
+        copyToClipboard(text, function() {
+            var node = $(element.childNodes[0])
+            node.attr('class', "fa fa-check")
+            setTimeout(function () { 
+                node.attr('class', "fas fa-copy")
+             } ,1000)
+            // fadeOutAndIn(node, function() {
+            //     node.attr('class', "fa fa-check")
+            // }, function() {
+            //     console.log(node.class)
+            //     setTimeout(function () { 
+            //         fadeOutAndIn(node, function() {
+            //             node.attr('class', "fas fa-copy")
+            //         },function () { 
+                        
+            //         })
+            //      } ,1000)
+            // })
+        })
+    } 
+}
+
+function fadeOutAndIn(node, onShow, onHide) {
+    node.fadeOut(200, function() {
+        onHide()
+        node.fadeIn(200, function () { 
+            onShow()
+         })
+    })
 }
 
